ab049f74bad2a795dae1f22dcfd9a9743384790c
maria <mary.smile4e@gmail.com>
2024-12-05 02:19:36 +0100

    fix per theme

--- setup.py
+++ setup.py
@@ -6,3 +6,19 @@ setup(
     description="Descrizione del programma",
     executables=[Executable("main.py", base=None)],
 )
+
+'''
+Genera il file eseguibile con:
+
+bash
+Copia codice
+python setup.py build
+Troverai il tuo .exe nella cartella build.
+
+2. Convertire in MSI Una volta ottenuto il file .exe, usa cx_Freeze per generare un file .msi:
+
+bash
+Copia codice
+python setup.py bdist_msi
+Troverai il file .msi nella cartella dist. Questo file può essere eseguito su altri computer per installare il tuo programma.
+'''
